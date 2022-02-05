46814e5fcec7378ec1ad5cc324fe4a0060ec5902
Meg <41833919+SpicyCayenne@users.noreply.github.com>
2022-02-04 23:06:09 -0800

    i hate math

--- MathChallenge.js
+++ MathChallenge.js
@@ -1,42 +1,41 @@
 class MathChallenge {
-    constructor() {
-        this.score = 0;
-    }
     play() {
         let problem = new Problem();
+        let score = 0
         document.getElementById('problem').innerText += problem.getExpression();
-
-        document.getElementById('myAnswer').addEventListener('keyup',(e) => {
-            if (e.key === "Enter") {
-                this.check(problem);}
+        document.getElementById('submit').addEventListener('click', function (e) {
+            let myAnswer = parseInt(document.getElementById('myAnswer').value);
+            let correctAnswer = problem.getAnswer();
+            if (myAnswer === correctAnswer) {
+                document.getElementById('problem').innerText = `Correct!\r`;
+                document.getElementById('myAnswer').value = null;
+                score++;
+                problem = new Problem();
+                document.getElementById('problem').innerText += problem.getExpression();
+            } else {
+                document.getElementById('problem').innerText = '';
+                document.getElementById('endGame').style.display = 'flex';
+                document.getElementById('question').style.display = 'none';
+                document.getElementById('message').innerText = `Game Over! Correct answer was ${correctAnswer}\rScore: ${score}`;
             }
-        )
-        document.getElementById('submit').addEventListener('click', (e) => {this.check(problem)});
+        });
     }
 
-    check(theProblem) {
+
+    /*check(theProblem) {
         let myAnswer = parseInt(document.getElementById('myAnswer').value);
         let correctAnswer = theProblem.getAnswer(); //on the second round, this is still using the value from the first round
         if (myAnswer === correctAnswer) {
             document.getElementById('problem').innerText = `Correct!\r`;
             document.getElementById('myAnswer').value = null;
-            document.getElementById('myAnswer').removeEventListener('keyup',(e) => {
-                if (e.key === "Enter") {
-                    this.check(problem);}
-                }
-            )
-            document.getElementById('submit').removeEventListener('click',() => {
-                this.check(problem);
-                }
-            )
             this.score++
-            this.play();
+            return this.play();
         } else {
             this.gameOver(correctAnswer, this.score);
         }
-    }
+    }*/
 
-    gameOver(correctAnswer, score) {
+    gameOver(correctAnswer) {
         document.getElementById('problem').innerText = '';
         document.getElementById('endGame').style.display = 'flex';
         document.getElementById('question').style.display = 'none';
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function newGame() {
     document.getElementById('endGame').style.display = 'none';
     document.getElementById('question').style.display = 'flex';
     document.getElementById('myAnswer').value = null;
-    return this.play();
+    location.reload();
 }
 document.getElementById('playAgain').addEventListener('click', () => {
     newGame();
